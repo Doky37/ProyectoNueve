812918468eda7bf5259aebca5416210838783884
UAML <uaml@iMac-de-UAML-3.local>
2024-09-12 10:38:15 -0600

    ljlsjs

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
 Console.log("Hola este es mi portafolio")
 
-let currentIndex = 0;
 
-const images = document.querySelectorAll('.carrusel-images img');
-const totalImages = images.length;
-
-function showSlide(index) {
-    const offset = -index * 100;
-    document.querySelector('.carrusel-images').style.transform = `translateX(${offset}%)`;
-}
-
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalImages;
-    showSlide(currentIndex);
-}
-
-function prevSlide() {
-    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-    showSlide(currentIndex);
-}
-
-// Opcional: Automatizar el cambio de imágenes
-setInterval(nextSlide, 3000); // Cambia la imagen cada 3 segundos
